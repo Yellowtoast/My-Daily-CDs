41b35e4f8a35390c4d9761faefa339f80ea4fcea
yellowtoast <kjh9519@naver.com>
2023-02-04 14:05:35 +0900

    edit: home view에 필요 없는 파일 수정

--- lib/data/view/home_view/home_view.dart
+++ lib/data/view/home_view/home_view.dart
@@ -1,11 +1,37 @@
+import 'dart:async';
+
 import 'package:daily_cd_player/core/theme/colors.dart';
 import 'package:daily_cd_player/core/theme/size.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
-
 import '../../../routes/routes.dart';
+import 'widgets/menu_button.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  void initState() {
+    super.initState();
+
+    // try {
+    //   final userCredential = FirebaseAuth.instance.signInAnonymously();
+    //   print("Signed in with temporary account.");
+    // } on FirebaseAuthException catch (e) {
+    //   switch (e.code) {
+    //     case "operation-not-allowed":
+    //       print("Anonymous auth hasn't been enabled for this project.");
+    //       break;
+    //     default:
+    //       print("Unknown error.");
+    //   }
+    // }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +48,7 @@ class HomeView extends StatelessWidget {
                 'assets/images/cd.png',
                 height: 125,
               ),
-              SizedBox(
+              const SizedBox(
                 height: 15,
               ),
               Image.asset(
@@ -44,8 +70,17 @@ class HomeView extends StatelessWidget {
                         child: HomeMenuButton(
                       imageAssetSrc: 'assets/images/multiple_cds.png',
                       menuLabel: '오늘의 CD 추천',
-                      onTap: () =>
-                          Navigator.pushNamed(context, Routes.RECOMMEND),
+                      onTap: () async {
+                        Navigator.pushNamed(context, Routes.RECOMMEND);
+
+                        await FirebaseAnalytics.instance.logEvent(
+                          name: "share_image",
+                          parameters: {
+                            "image_name": 'asdf',
+                            "full_text": 'asdfadsf',
+                          },
+                        );
+                      },
                     )),
                     const SizedBox(
                       width: 20,
@@ -58,59 +93,11 @@ class HomeView extends StatelessWidget {
                     ))
                   ],
                 ),
-              )
+              ),
             ],
           ),
         ),
       )),
-    );
-  }
-}
-
-class HomeMenuButton extends StatelessWidget {
-  const HomeMenuButton({
-    Key? key,
-    required String imageAssetSrc,
-    required void Function() onTap,
-    required String menuLabel,
-  })  : _imageAsset = imageAssetSrc,
-        _onTap = onTap,
-        _menuLabel = menuLabel,
-        super(key: key);
-
-  final String _imageAsset;
-  final void Function() _onTap;
-  final String _menuLabel;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _onTap,
-      child: Column(
-        children: [
-          Container(
-            height: 135,
-            alignment: Alignment.center,
-            padding: const EdgeInsets.symmetric(vertical: 25),
-            decoration: BoxDecoration(
-                color: MyColors.white,
-                border: Border.all(color: MyColors.black)),
-            child: Image.asset(
-              _imageAsset,
-              // height: 90,
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            color: MyColors.black,
-            height: 35,
-            child: Text(
-              _menuLabel,
-              style: Theme.of(context).typography.white.bodyText1,
-            ),
-          )
-        ],
-      ),
     );
   }
 }
